f3c803d7f3ffa45b8ae565997f0c55da4d1d8f24
dairongranada <dairongranada21@gmail.com>
2022-12-21 17:54:02 -0500

    Cambios de la firma -DG

--- app2.js
+++ app2.js
@@ -4,22 +4,26 @@ OpenModalOne.addEventListener("click", ModalOneFuntion);
 const OpenModalTwo = document.getElementById("OpenModalTwo");
 OpenModalTwo.addEventListener("click", ModalTwoFuntion);
 
+const OpenModalThree = document.getElementById("OpenModalThree");
+OpenModalThree.addEventListener("click", ModalThreeFuntion);
 
 
+
+window.requestAnimFrame = (function (callback) {
+	return window.requestAnimationFrame ||
+				window.webkitRequestAnimationFrame ||
+				window.mozRequestAnimationFrame ||
+				window.oRequestAnimationFrame ||
+				window.msRequestAnimaitonFrame ||
+				function (callback) {
+					 window.setTimeout(callback, 1000/60);
+		
+				};
+})();
 
 
 function ModalOneFuntion() { 
-	window.requestAnimFrame = (function (callback) {
-		return window.requestAnimationFrame ||
-					window.webkitRequestAnimationFrame ||
-					window.mozRequestAnimationFrame ||
-					window.oRequestAnimationFrame ||
-					window.msRequestAnimaitonFrame ||
-					function (callback) {
-					 	window.setTimeout(callback, 1000/60);
-            
-					};
-	})();
+
 
 	console.log("arlettttttttttttttt  2dooooooooooooo");
 
@@ -161,17 +165,7 @@ function ModalOneFuntion() {
 //  F I R M A   2da
 
 function ModalTwoFuntion() { 
-	window.requestAnimFrame = (function (callback) {
-		return window.requestAnimationFrame ||
-					window.webkitRequestAnimationFrame ||
-					window.mozRequestAnimationFrame ||
-					window.oRequestAnimationFrame ||
-					window.msRequestAnimaitonFrame ||
-					function (callback) {
-					 	window.setTimeout(callback, 1000/60);
-            
-					};
-	})();
+
 
 
 
@@ -302,10 +296,150 @@ function ModalTwoFuntion() {
 		canvasTwo.width = canvasTwo.width;
 	}
 
-
 	(function drawLoop () {
 		requestAnimFrame(drawLoop);
 		renderCanvas();
 	})();
 
 };
+
+
+
+
+function ModalThreeFuntion() { 
+
+	console.log("arlettttttttttttttt  3tresssssssssssssss ");
+
+	var canvasTwo = document.getElementById("Threedrawcanvas");
+	var ctxTwo = canvasTwo.getContext("2d");
+	ctxTwo.font = "bold 22px sans-serif";
+	ctxTwo.fillText("Firmar",15,30)
+
+	ctxTwo.fillText("___________________________________________",10,160)
+
+	var TwodrawText = document.getElementById("ThreedrawdataUrl");
+	var TwodrawImage = document.getElementById("Threedrawimage");
+	var TwoclearBtn = document.getElementById("ThreedrawclearBtn");
+	var TwosubmitBtn = document.getElementById("ThreedrawsubmitBtn");
+
+
+	TwoclearBtn.addEventListener("click", function (e) {
+		clearCanvas();
+		TwodrawImage.setAttribute("src", "../img/undraw_terms_re_6ak4.svg");
+		TwodrawText.innerHTML = '';
+		ctxTwo.font = "bold 22px sans-serif";
+		ctxTwo.fillText("Firmar*",15,30)
+		ctxTwo.fillText("___________________________________________",10,160)
+
+	}, false);
+		
+
+
+
+	TwosubmitBtn.addEventListener("click", function (e) {
+	var dataUrl = canvasTwo.toDataURL();
+
+	
+	TwodrawText.innerHTML = dataUrl;
+	TwodrawImage.setAttribute("src", dataUrl);
+	 }, false);
+
+	
+	var drawing = false;
+	var mousePos = { x:0, y:0 };
+	var lastPos = mousePos;
+	canvasTwo.addEventListener("mousedown", function (e)
+  {
+		drawing = true;
+		lastPos = getMousePos(canvasTwo, e);
+	}, false);
+	canvasTwo.addEventListener("mouseup", function (e)
+  {
+		drawing = false;
+	}, false);
+	canvasTwo.addEventListener("mousemove", function (e)
+  {
+		mousePos = getMousePos(canvasTwo, e);
+	}, false);
+
+
+	canvasTwo.addEventListener("touchstart", function (e) {
+		mousePos = getTouchPos(canvasTwo, e);
+    e.preventDefault(); 
+		var touch = e.touches[0];
+		var mouseEvent = new MouseEvent("mousedown", {
+			clientX: touch.clientX,
+			clientY: touch.clientY
+		});
+		canvasTwo.dispatchEvent(mouseEvent);
+	}, false);
+	canvasTwo.addEventListener("touchend", function (e) {
+    e.preventDefault(); 
+		var mouseEvent = new MouseEvent("mouseup", {});
+		canvasTwo.dispatchEvent(mouseEvent);
+	}, false);
+  canvasTwo.addEventListener("touchleave", function (e) {
+    
+    e.preventDefault();
+    var mouseEvent = new MouseEvent("mouseup", {});
+    canvasTwo.dispatchEvent(mouseEvent);
+  }, false);
+	canvasTwo.addEventListener("touchmove", function (e) {
+    e.preventDefault(); 
+		var touch = e.touches[0];
+		var mouseEvent = new MouseEvent("mousemove", {
+			clientX: touch.clientX,
+			clientY: touch.clientY
+		});
+		canvasTwo.dispatchEvent(mouseEvent);
+	}, false);
+
+	
+	function getMousePos(canvasDom, mouseEvent) {
+		var rect = canvasDom.getBoundingClientRect();
+
+		return {
+			x: mouseEvent.clientX - rect.left,
+			y: mouseEvent.clientY - rect.top
+		};
+	}
+
+	
+	function getTouchPos(canvasDom, touchEvent) {
+		var rect = canvasDom.getBoundingClientRect();
+
+		return {
+			x: touchEvent.touches[0].clientX - rect.left, 
+			y: touchEvent.touches[0].clientY - rect.top
+		};
+	}
+
+
+	function renderCanvas() {
+	if (drawing) {
+      var tint = "#000";
+      var punta = 3;
+      ctxTwo.strokeStyle = tint;
+      ctxTwo.beginPath();
+			ctxTwo.moveTo(lastPos.x, lastPos.y);
+			ctxTwo.lineTo(mousePos.x, mousePos.y);
+    	ctxTwo.lineWidth = punta;
+			ctxTwo.stroke();
+      ctxTwo.closePath();
+
+			lastPos = mousePos;
+		}
+	}
+
+	function clearCanvas() {
+		canvasTwo.width = canvasTwo.width;
+	}
+
+	(function drawLoop () {
+		requestAnimFrame(drawLoop);
+		renderCanvas();
+	})();
+
+
+}
+
